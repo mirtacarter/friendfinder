941fa6801886c5f2425ca6d1c442e6ff362b35f7
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-08-23 23:36:57 -0400

    main logic and layout

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var friendsArray = [
+    {
+name: "Monkey Cat",
+photo: "https://media.giphy.com/media/xT77XHUkpCtKbmtuH6/giphy.gif", 
+scores: [
+
+]
+    },
+    {
+    name: "Boat Cat",
+photo: "https://media.giphy.com/media/okf9AZSgOxT8s/giphy.gif", 
+scores: [
+
+]
+    },
+    {
+    name: "Keyboard Cat",
+photo: "https://media.giphy.com/media/Hcw7rjsIsHcmk/giphy.gif", 
+scores: [
+
+]
+    },
+    {
+    name: "Scuba Cat",
+photo: "https://media.giphy.com/media/AheZDLIrOKu2Y/giphy.gif", 
+scores: [
+
+]
+    },
+    {
+    name: "Chicken Cat",
+photo: "https://media.giphy.com/media/12PA1eI8FBqEBa/giphy.gif", 
+scores: [
+
+]
+    },
+
+  ];
+  
+  //Export array
+  module.exports = friendsArray;
+  
