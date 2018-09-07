808894cb2300a264e7aad7b3ed1854d5bd69c476
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-09-07 16:40:35 -0400

    updated apiRoutes to include scoring algorithm, added results modal to survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,45 +2,90 @@
 // DATA
 // ===============================================================================
 
-var friendsArray = [
+var friendsData = [
     {
 name: "Monkey Cat",
 photo: "https://media.giphy.com/media/xT77XHUkpCtKbmtuH6/giphy.gif", 
 scores: [
-
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
 ]
     },
     {
     name: "Boat Cat",
 photo: "https://media.giphy.com/media/okf9AZSgOxT8s/giphy.gif", 
 scores: [
-
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
 ]
     },
     {
     name: "Keyboard Cat",
 photo: "https://media.giphy.com/media/Hcw7rjsIsHcmk/giphy.gif", 
 scores: [
-
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
 ]
     },
     {
     name: "Scuba Cat",
 photo: "https://media.giphy.com/media/AheZDLIrOKu2Y/giphy.gif", 
 scores: [
-
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
 ]
     },
     {
     name: "Chicken Cat",
 photo: "https://media.giphy.com/media/12PA1eI8FBqEBa/giphy.gif", 
 scores: [
-
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
 ]
     },
 
   ];
   
   //Export array
-  module.exports = friendsArray;
+  module.exports = friendsData;
   
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -20,7 +20,50 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
-      res.json(true);
+    // BFF Object
+    var bff = {
+      name: "",
+      photo: "",
+      matchScore: Infinity
+    };
+
+    // Grab survey scores for comparison with friendsArray scores
+    var surveyAnswers = req.body;
+    var surveyScores = surveyAnswers.scores;
+    
+    // Algorithm calculation variable
+    var scoreCalc;
+
+    // Scoring algorithm
+    for (var i = 0; i < friendsData.length; i++) {
+      var potentialBff = friendsData[i];
+      scoreCalc = 0;
+
+      console.log(potentialBff.name);
+
+      // loop through all the scores of each friend
+      for (var j = 0; j < potentialBff.scores.length; j++) {
+        var potentialBffScore = potentialBff.scores[j];
+        var currentScore = surveyScores[j];
+
+        // calculate the difference between the scores, add them to get the final score calculation
+        scoreCalc += Math.abs(parseInt(currentScore) - parseInt(potentialBffScore));
+      }
+
+      // If the sum of differences is less then the differences of the current bff
+      if (scoreCalc <= bff.matchScore) {
+        // Reset the bff to be the new friend.
+        bff.name = potentialBff.name;
+        bff.photo = potentialBff.photo;
+        bff.matchScore = scoreCalc;
+      }
+    }
+
+    // save data to the database
+    friends.push(surveyAnswers);
+
+    // Return bff json to render to page
+    res.json(bff);
+
   });
 }
